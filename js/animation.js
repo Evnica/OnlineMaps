904f30f52d42a11386a0c7f82222e516bd52cb5d
Evnica <evnica@gmail.com>
2016-04-17 23:09:06 +0200

    add spinner animation for play button add dynamical kml loading for play button

--- js/animation.js
+++ js/animation.js
@@ -1,72 +1,61 @@
+
 /**
  * Inspired by html5demos: https://github.com/remy/html5demos/blob/master/demos/canvas.html
  * Adapted by Evnica on 13.04.2016.
  */
 
-function drawTime(context, radius){
-    var now = new Date();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    //hour
-    hour=hour%12;
-    hour=(hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
-    drawHand(context, hour, radius*0.5, radius*0.07);
-    //minute
-    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-    drawHand(context, minute, radius*0.8, radius*0.07);
-    // second
-    second=(second*Math.PI/30);
-    drawHand(context, second, radius*0.9, radius*0.02);
-}
-
-
+var degree = 0;
 
 function createSpinner(data) {
 
     var canvas = document.getElementById('clockSpinner'); //locate the spinner
     var context = canvas.getContext("2d"),
-        i = 0, //counter
+        i = degree, //counter
         degrees = 60,           //number of sectors to draw in a circle
 
         degreesList = [];       //list of ordinal numbers of sectors
-        for (i = 0; i < degrees; i++)
+        for (var j = 0; j < 21; j++) //only first 20 minutes will be displayed
         {
-            degreesList.push(i); //add to an array numbers from 0 to degrees-1 (0 to 59, 1 item per second)
+            degreesList.push(j); //add to an array numbers from 0 to degrees-1 (0 to 59, 1 item per second)
         }
 
-    i = 0; //reset counter
 
     window.canvasTimer = setInterval(draw, 1000); //set the frequency of picture appearance 1 per second
 
-
     function draw() {
-        var color, start, end;
-        var currentDegree;
-        if (i == 0)
+        if(proceed==true)
         {
-            reset();
-        }
-        context.save();
-        currentDegree = degreesList[i]; //0 in the first iteration
-        color = Math.floor(255/degrees*i*2.5);
-        context.strokeStyle = 'rgb(' + color + ', ' + 210 + ', ' + 0 + ')';
+            var color, start, end;
+            var currentDegree;
+            if (i == 0)
+            {
+                reset();
+            }
+            context.save();
+            currentDegree = degreesList[i]; //0 in the first iteration
 
-        context.lineWidth = data.size;
-        context.beginPath();
-        start = Math.floor(360/degrees*(currentDegree) - 90); //start at the top: 90 degrees, 12 o'clock
-        end = Math.floor(360/degrees*(currentDegree+1)) - 89;
-        //context.arc(x, y, r, sAngle, eAngle, counterclockwise);
-        context.arc(data.x, data.y, data.size, (Math.PI/180)*start, (Math.PI/180)*end, false);
-        context.stroke();
-        context.restore();
-        i++;
-        if (i >= degrees)
-        {
-            i = 0;
+            color = Math.floor(255/degrees*i*2.5);
+            context.strokeStyle = 'rgb(' + color + ', ' + 210 + ', ' + 0 + ')';
+
+            context.lineWidth = data.size;
+            context.beginPath();
+            start = Math.floor(360/degrees*(currentDegree) - 90); //start at the top: 90 degrees, 12 o'clock
+            end = Math.floor(360/degrees*(currentDegree+1)) - 89;
+            //context.arc(x, y, r, sAngle, eAngle, counterclockwise);
+            context.arc(data.x, data.y, data.size, (Math.PI/180)*start, (Math.PI/180)*end, false);
+            context.stroke();
+            context.restore();
+            i++;
+            degree++;
+            if (i >= 20)
+            {
+                i = 0;
+            }
         }
-    }
-    function reset() {
-        context.clearRect(0,0,150,150); // clear canvas around the spinner
+
+        function reset() {
+            context.clearRect(0,0,150,150); // clear canvas of the spinner
+            degree = 0;
+        }
     }
 }
--- js/map.js
+++ js/map.js
@@ -5,13 +5,24 @@
  * Version: 0.1
  */
 
-$.getScript("http://maps.google.com/maps/api/js?sensor=true&language=de&region=DE&async=2&callback=MapApiLoaded", function () {});
+var map, rescueLayer, currentIndex = 1, iterator = 1, kmlOptions;
+var proceed = false;
+
 function initMap()
 {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 46.75, lng: 13.87},
-        zoom: 9
-    });
+    map = new google.maps.Map(document.getElementById('map'),
+        {
+            center: {lat: 46.75, lng: 13.87},
+            zoom: 9,
+            mapTypeControl: false,
+            /*mapTypeControlOptions:
+             {
+             style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+             position: google.maps.ControlPosition.BOTTOM_LEFT
+             },*/
+            scaleControl: true,
+            streetViewControl: false
+        }                        );
     map.set('styles', [
         {
             featureType: 'road',
@@ -22,11 +33,61 @@ function initMap()
         }
     ]);
 
-    var kmlOptions = {
+    kmlOptions = {
         suppressInfoWindows: true,
         preserveViewport: true,
         map:map
     };
+
     var kmlLayerFrame = new google.maps.KmlLayer("http://evnica.com/kml/frameDark.kmz", kmlOptions);
     var kmlLayerBorder = new google.maps.KmlLayer("http://evnica.com/kml/kaernten.kml", kmlOptions);
 }
+
+function loadRescueStations()
+{
+    var kmlLayerStation = new google.maps.KmlLayer("http://evnica.com/kml/rescueStations.kmz", kmlOptions);
+}
+
+
+function loadRescueKml(i)
+{
+    var url = "http://evnica.com/kml/" + i + "min.kmz";
+    var rescueLayer = new google.maps.KmlLayer(url, kmlOptions);
+}
+
+function pause()
+{
+    proceed = false;
+}
+
+function playRescue()
+{
+    proceed = true;
+    setTimeout(loadRescueStations(), 500);
+    createSpinner({ x : 75, y : 75, size : 44});
+
+    for (var i=iterator; i<21; i++) {
+        if (proceed == true)
+        {
+            setTimeout(function() {
+                loadRescueKml(currentIndex);
+                currentIndex ++;
+            }, 1000*i);
+        }
+        else
+        {
+            iterator = currentIndex;
+            alert('dfk');
+        }
+    }
+
+
+
+}
+
+
+
+
+
+
+
