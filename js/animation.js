6fca8846d9635b019b25b3ba5bbc48755b16da57
Evnica <evnica@gmail.com>
2016-04-17 11:17:39 +0200

    add basic kml to the map and remove spinner

--- js/animation.js
+++ js/animation.js
@@ -3,7 +3,26 @@
  * Adapted by Evnica on 13.04.2016.
  */
 
-function buildSpinner(data) {
+function drawTime(context, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+    drawHand(context, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(context, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+    drawHand(context, second, radius*0.9, radius*0.02);
+}
+
+
+
+function createSpinner(data) {
 
     var canvas = document.getElementById('clockSpinner'); //locate the spinner
     var context = canvas.getContext("2d"),
