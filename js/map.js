1fad03c1609725c6c0200111a1efb58dbadd5ab6
Evnica <evnica@gmail.com>
2016-04-19 13:27:05 +0200

    animate layers and spinner, pause spinner, animation.js deleted

--- js/map.js
+++ js/map.js
@@ -5,8 +5,15 @@
  * Version: 0.1
  */
 
-var map, rescueLayer, currentIndex = 1, iterator = 1, kmlOptions;
-var proceed = false;
+var map,
+    timeout,
+    rescueStationsLayer,
+    currentImg = 1, //Number of img currently displayed
+    iterator = 1,     //
+    kmlOptions,
+    proceed = false,
+    wasPausedBeforePlaying = false,
+    layers = [];
 
 function initMap()
 {
@@ -41,48 +48,125 @@ function initMap()
 
     var kmlLayerFrame = new google.maps.KmlLayer("http://evnica.com/kml/frameDark.kmz", kmlOptions);
     var kmlLayerBorder = new google.maps.KmlLayer("http://evnica.com/kml/kaernten.kml", kmlOptions);
+    /*rescuePolygonLayer = new google.maps.KmlLayer(kmlOptions);*/
+    rescueStationsLayer = new google.maps.KmlLayer(kmlOptions);
 }
-
-function loadRescueStations()
-{
-    var kmlLayerStation = new google.maps.KmlLayer("http://evnica.com/kml/rescueStations.kmz", kmlOptions);
-}
-
 
 function loadRescueKml(i)
 {
-    var url = "http://evnica.com/kml/" + i + "min.kmz";
-    var rescueLayer = new google.maps.KmlLayer(url, kmlOptions);
+    if (proceed == true && i < 21)
+    {
+        var url = "http://evnica.com/kml/" + i + "min.kmz";
+        layers.push(new google.maps.KmlLayer(url, kmlOptions));
+        /*rescuePolygonLayer.setUrl(url);*/
+    }
 }
 
 function pause()
 {
     proceed = false;
+    iterator = currentImg;
+    wasPausedBeforePlaying = true;
 }
 
 function playRescue()
 {
-    proceed = true;
-    setTimeout(loadRescueStations(), 500);
-    createSpinner({ x : 75, y : 75, size : 44});
-
-    for (var i=iterator; i<21; i++) {
-        if (proceed == true)
+    if (proceed == false)       //if pause was pressed before or the site was just loaded
+    {
+        proceed = true;         // indicate it's time to start
+        if (currentImg > 20)    //if all the images have been already shown
         {
-            setTimeout(function() {
-                loadRescueKml(currentIndex);
-                currentIndex ++;
-            }, 1000*i);
+            currentImg = 1; //point img to the first one
         }
-        else
+
+        if (currentImg == 1) //if it's the start of animation, load rescue stations locations
         {
-            iterator = currentIndex;
-            alert('dfk');
+            rescueStationsLayer.setUrl("http://evnica.com/kml/rescueStations.kmz");
+        }
+        var i = currentImg; // iteration through images starts with the last img remembered
+
+        startSpinner({x: 75, y: 75, size: 44});
+
+        while (i < 21) // iterate through images
+        {
+            if (proceed == true) // pause could be pressed in the meanwhile
+            {
+                timeout = setTimeout(function () {
+                    loadRescueKml(currentImg);
+                    currentImg++;
+                }, 1000 * i);
+                i++;
+            }
+            else {
+                break;
+            }
         }
     }
 
+}
 
 
+/**
+ * Inspired by html5demos: https://github.com/remy/html5demos/blob/master/demos/canvas.html
+ * Adapted by Evnica on 13.04.2016.
+ */
+
+var degree = 0;
+
+function startSpinner (data)
+{
+    setTimeout(createSpinner(data), 1000);
+}
+
+function createSpinner(data)
+{
+    var canvas = document.getElementById('clockSpinner'); //locate the spinner
+    var context = canvas.getContext("2d");
+    var i = degree, //counter
+        degreesList = [];       //list of ordinal numbers of sectors
+    for (var j = 0; j < 21; j++) //only first 20 minutes will be displayed
+    {
+        degreesList.push(j);
+    }
+
+
+    window.canvasTimer = setInterval(draw, 1000); //set the frequency of picture appearance 1 per second
+
+    function draw() {
+        if(proceed==true)
+        {
+            var color, start, end;
+            var currentDegree;
+            context.save();
+            currentDegree = degreesList[i]; //0 in the first iteration
+
+            color = Math.floor(255/60*i*2.5);
+            context.strokeStyle = 'rgb(' + color + ', ' + 210 + ', ' + 0 + ')';
+
+            context.lineWidth = data.size;
+            context.beginPath();
+            start = Math.floor(6*(currentDegree) - 90); //start at the top: 90 degrees, 12 o'clock
+            end = Math.floor(6*(currentDegree+1) - 90);
+            //context.arc(x, y, r, sAngle, eAngle, counterclockwise);
+            context.arc(data.x, data.y, data.size, (Math.PI/180)*start, (Math.PI/180)*end, false);
+            context.stroke();
+            context.restore();
+            i++;
+            degree++;
+            if (i > 20)
+            {
+                i = 0;
+                proceed = false;
+                reset();
+            }
+        }
+
+        function reset() {
+            context.clearRect(0,0,150,150); // clear canvas of the spinner
+            degree = 0;
+            clearInterval(window.canvasTimer);
+        }
+    }
 }
 
 
