2521d3bdb516a5565c62cf9bd541e7b9b35ec126
Evnica <evnica@gmail.com>
2016-04-19 20:38:50 +0200

    synchronous animation of spinner and img; pause stops both, play resumes correctly

--- js/map.js
+++ js/map.js
@@ -6,14 +6,34 @@
  */
 
 var map,
-    timeout,
+    degree = 0,
     rescueStationsLayer,
-    currentImg = 1, //Number of img currently displayed
-    iterator = 1,     //
     kmlOptions,
-    proceed = false,
-    wasPausedBeforePlaying = false,
-    layers = [];
+    proceed = false;
+var layers = []
+/*{
+        serviceArea01: {url : "http://evnica.com/kml/1min.kmz"},
+        serviceArea02: {url : "http://evnica.com/kml/2min.kmz"},
+        serviceArea03: {url : "http://evnica.com/kml/3min.kmz"},
+        serviceArea04: {url : "http://evnica.com/kml/4min.kmz"},
+        serviceArea05: {url : "http://evnica.com/kml/5min.kmz"},
+        serviceArea06: {url : "http://evnica.com/kml/6min.kmz"},
+        serviceArea07: {url : "http://evnica.com/kml/7min.kmz"},
+        serviceArea08: {url : "http://evnica.com/kml/8min.kmz"},
+        serviceArea09: {url : "http://evnica.com/kml/9min.kmz"},
+        serviceArea10: {url : "http://evnica.com/kml/10min.kmz"},
+        serviceArea11: {url : "http://evnica.com/kml/11min.kmz"},
+        serviceArea12: {url : "http://evnica.com/kml/12min.kmz"},
+        serviceArea13: {url : "http://evnica.com/kml/13min.kmz"},
+        serviceArea14: {url : "http://evnica.com/kml/14min.kmz"},
+        serviceArea15: {url : "http://evnica.com/kml/15min.kmz"},
+        serviceArea16: {url : "http://evnica.com/kml/16min.kmz"},
+        serviceArea17: {url : "http://evnica.com/kml/17min.kmz"},
+        serviceArea18: {url : "http://evnica.com/kml/18min.kmz"},
+        serviceArea19: {url : "http://evnica.com/kml/19min.kmz"},
+        serviceArea20: {url : "http://evnica.com/kml/20min.kmz"}
+}*/;
+var playedCompletely = false;
 
 function initMap()
 {
@@ -48,8 +68,6 @@ function initMap()
 
     var kmlLayerFrame = new google.maps.KmlLayer("http://evnica.com/kml/frameDark.kmz", kmlOptions);
     var kmlLayerBorder = new google.maps.KmlLayer("http://evnica.com/kml/kaernten.kml", kmlOptions);
-    /*rescuePolygonLayer = new google.maps.KmlLayer(kmlOptions);*/
-    rescueStationsLayer = new google.maps.KmlLayer(kmlOptions);
 }
 
 function loadRescueKml(i)
@@ -58,113 +76,96 @@ function loadRescueKml(i)
     {
         var url = "http://evnica.com/kml/" + i + "min.kmz";
         layers.push(new google.maps.KmlLayer(url, kmlOptions));
-        /*rescuePolygonLayer.setUrl(url);*/
+        //layers[i].obj = new google.maps.KmlLayer(layers[i].url, kmlOptions);
     }
 }
 
 function pause()
 {
-    proceed = false;
-    iterator = currentImg;
-    wasPausedBeforePlaying = true;
-}
-
-function playRescue()
-{
-    if (proceed == false)       //if pause was pressed before or the site was just loaded
+    if (proceed == true)
     {
-        proceed = true;         // indicate it's time to start
-        if (currentImg > 20)    //if all the images have been already shown
-        {
-            currentImg = 1; //point img to the first one
-        }
-
-        if (currentImg == 1) //if it's the start of animation, load rescue stations locations
-        {
-            rescueStationsLayer.setUrl("http://evnica.com/kml/rescueStations.kmz");
-        }
-        var i = currentImg; // iteration through images starts with the last img remembered
-
-        startSpinner({x: 75, y: 75, size: 44});
-
-        while (i < 21) // iterate through images
-        {
-            if (proceed == true) // pause could be pressed in the meanwhile
-            {
-                timeout = setTimeout(function () {
-                    loadRescueKml(currentImg);
-                    currentImg++;
-                }, 1000 * i);
-                i++;
-            }
-            else {
-                break;
-            }
-        }
+        proceed = false;
+        clearInterval(window.canvasTimer);
     }
-
 }
-
 
 /**
- * Inspired by html5demos: https://github.com/remy/html5demos/blob/master/demos/canvas.html
+ * The further code was inspired by html5demos: https://github.com/remy/html5demos/blob/master/demos/canvas.html
  * Adapted by Evnica on 13.04.2016.
  */
 
-var degree = 0;
-
-function startSpinner (data)
-{
-    setTimeout(createSpinner(data), 1000);
-}
-
-function createSpinner(data)
-{
-    var canvas = document.getElementById('clockSpinner'); //locate the spinner
-    var context = canvas.getContext("2d");
-    var i = degree, //counter
-        degreesList = [];       //list of ordinal numbers of sectors
-    for (var j = 0; j < 21; j++) //only first 20 minutes will be displayed
+function startRescueAnimation(data) {
+    if (proceed == false) // don't play if is already playing
     {
-        degreesList.push(j);
-    }
-
-
-    window.canvasTimer = setInterval(draw, 1000); //set the frequency of picture appearance 1 per second
-
-    function draw() {
-        if(proceed==true)
+        var i = degree,//counter, doesn't always start with 0, as scenario could be stopped in the middle
+            degreesList = [];       //list of ordinal numbers of sectors
+        for (var j = 0; j < 20; j++) //only first 20 minutes will be displayed
         {
-            var color, start, end;
-            var currentDegree;
-            context.save();
-            currentDegree = degreesList[i]; //0 in the first iteration
-
-            color = Math.floor(255/60*i*2.5);
-            context.strokeStyle = 'rgb(' + color + ', ' + 210 + ', ' + 0 + ')';
-
-            context.lineWidth = data.size;
-            context.beginPath();
-            start = Math.floor(6*(currentDegree) - 90); //start at the top: 90 degrees, 12 o'clock
-            end = Math.floor(6*(currentDegree+1) - 90);
-            //context.arc(x, y, r, sAngle, eAngle, counterclockwise);
-            context.arc(data.x, data.y, data.size, (Math.PI/180)*start, (Math.PI/180)*end, false);
-            context.stroke();
-            context.restore();
-            i++;
-            degree++;
-            if (i > 20)
-            {
-                i = 0;
-                proceed = false;
-                reset();
-            }
+            degreesList.push(j);
         }
 
-        function reset() {
-            context.clearRect(0,0,150,150); // clear canvas of the spinner
+        if (i == 0) // if stations layer wasn't loaded before, load it
+        {
+            rescueStationsLayer = new google.maps.KmlLayer("http://evnica.com/kml/rescueStations.kmz", kmlOptions);
+        }
+        proceed = true;
+        var canvas = document.getElementById('clockSpinner'); //locate the spinner
+        var context = canvas.getContext("2d");
+
+        function reset()
+        {
+            context.clearRect(0, 0, 150, 150); // clear canvas of the spinner
             degree = 0;
             clearInterval(window.canvasTimer);
+            /*for (var item in layers)
+            {
+                if (layers[item].obj)
+                {
+                    layers[item].obj.setMap(null);
+                    delete layers[item].obj;
+                }
+            }*/
+        }
+
+        if (playedCompletely == true) // if it's a new start after the scenario was completely played before and images are still on the screen
+        {
+            reset(); // remove images
+            playedCompletely = false; // reset the flag
+        }
+
+        window.canvasTimer = setInterval(draw, 1000); //set the frequency of img appearance: 1 per second
+
+        function draw() {
+            if (proceed == true) //noone has stopped scenario before
+            {
+                var color, start, end;
+                var currentDegree;
+                context.save();
+                currentDegree = degreesList[i];
+
+                color = Math.floor(255 / 60 * i * 2.5); // shade of red for the RGB color
+                context.strokeStyle = 'rgb(' + color + ', ' + 210 + ', ' + 0 + ')';
+
+                context.lineWidth = data.size; // radius of the sector
+                context.beginPath();
+                start = Math.floor(6 * (currentDegree) - 90); //start at the top: -90 degrees, 12 o'clock
+                end = Math.floor(6 * (currentDegree + 1) - 90); // and at -84 degrees, which corresponds to 1 minute
+                //context.arc(x, y, r, sAngle, eAngle, counterclockwise);
+                context.arc(data.x, data.y, data.size, (Math.PI / 180) * start, (Math.PI / 180) * end, false);
+                context.stroke();
+                context.restore();
+
+                i++; // increase iterator to
+                loadRescueKml(i);
+                degree++; // save current degree value outside this method in case it's stopped by user to be able to
+                            //resume from the point where paused
+                if (i > 20) // 20 minutes covered
+                {
+                    degree = 0;
+                    proceed = false;
+                    playedCompletely = true;
+                }
+            }
         }
     }
 }
